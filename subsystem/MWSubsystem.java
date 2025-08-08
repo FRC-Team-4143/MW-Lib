cbf9c9bb681bb887b2cfd154f2bc88b2cee96eee
ColeHunt <huntcr2001@gmail.com>
2025-08-08 02:42:00 -0500

    wip: compiles but has error with logger

--- subsystem/MWSubsystem.java
+++ subsystem/MWSubsystem.java
@@ -1,9 +1,8 @@
 package frc.mw_lib.subsystem;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import monologue.Logged;
 
-public abstract class Subsystem extends SubsystemBase {
+public abstract class MWSubsystem implements Logged {
 
   /** Reads all sensors and stores periodic data */
   public abstract void readPeriodicInputs(double timestamp);
@@ -16,9 +15,6 @@ public abstract class Subsystem extends SubsystemBase {
 
   /** Outputs all logging information to the SmartDashboard */
   public abstract void outputTelemetry(double timestamp);
-
-  /** Get logging object from subsystem */
-  public abstract Logged getLoggingObject();
 
   /** Called to reset and configure the subsystem */
   public abstract void reset();
