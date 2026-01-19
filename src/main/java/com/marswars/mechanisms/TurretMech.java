2d94f3bcfcf4713194746ea66c2c7eab3857efd6
ColeHunt <huntcr2001@gmail.com>
2026-01-19 16:35:00 -0600

    makes turret mech constructor public

--- src/main/java/com/marswars/mechanisms/TurretMech.java
+++ src/main/java/com/marswars/mechanisms/TurretMech.java
@@ -5,7 +5,7 @@ import java.util.List;
 import com.marswars.util.FxMotorConfig;
 
 public class TurretMech extends RollerMech {
-    TurretMech(
+    public TurretMech(
             String logging_prefix,
             List<FxMotorConfig> motor_configs,
             double gear_ratio,
